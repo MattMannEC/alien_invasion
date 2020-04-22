c08d307b180ba20dac35505100ffb9560276dfad
mattmannec <mattmannec@gmail.com>
2020-04-22 14:02:04 +0200

    added settings class

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,16 +1,16 @@
 import sys
-
 import pygame
+from settings import Settings
 
 class AlienInvasion:
     #do you always init self in python ?
     def __init__(self):
 
         pygame.init()
-        # each element of a game should have it's own surface (ex ship, alien, star)
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+        # each element of a game should have it's own surface
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """ Active flag for game """
@@ -22,9 +22,9 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-                self.screen.fill(self.bg_color)
-
             #deletes the old screen and generates a new one taking into account the new events
+
+            self.screen.fill(self.settings.bg_color)
             pygame.display.flip()
 
 if __name__ == '__main__':
