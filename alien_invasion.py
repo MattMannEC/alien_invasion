1dd20c58cc81c978f77974e053f9930845de8136
mattmannec <mattmannec@gmail.com>
2020-04-22 13:46:10 +0200

    added background color

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,20 +7,24 @@ class AlienInvasion:
     def __init__(self):
 
         pygame.init()
-
+        # each element of a game should have it's own surface (ex ship, alien, star)
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """ Active flag for game """
         while True:
             # Listen for keyboard and mouse events
             # get() handles for none
+            # event returns an object of events that occured since the last while iteration
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            # Make the most recently drawn screen visible.
+                self.screen.fill(self.bg_color)
+
+            #deletes the old screen and generates a new one taking into account the new events
             pygame.display.flip()
 
 if __name__ == '__main__':
